e511f1c1120612ab2ab2afa20b63dbb976d0b532
Tatiana Goishik <69108210+Tati1909@users.noreply.github.com>
2022-04-20 23:20:54 +0300

    Cоздана новая ветка feature_layout

--- app/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/calculator/MainActivity.kt
+++ app/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/calculator/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.pet.animal.formula.dose.health.veterinary.cure.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //создана ветка feature_layout
     }
 }
